b7c8f7e71fb081a0d26a78477074d35d0a9faad6
BesteSakar <bestesakarr@outlook.com>
2022-05-16 14:19:32 +0300

    .NETCORE-MVC-CRUD-ISLEMLERI-CODE-FIRST--main

---
+++ .NETCORE-MVC-CRUD-ISLEMLERI-CODE-FIRST--main/HotelSearch/HotelFinder.DataAccess/HotelDbContext.cs
@@ -0,0 +1,22 @@
+﻿using HotelFinder.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace HotelFinder.DataAccess
+{
+    public class HotelDbContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=DESKTOP-FHSN1O8; Database=HotelDb; Trusted_Connection=true");
+
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+
+    }
+        
+    }
+
